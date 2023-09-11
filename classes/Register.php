9efab319c240e6c51fc45e84d3f3f122de5231ba
shoyab <abumansurshoyab@gmail.com>
2023-09-11 11:20:17 +0600

    Edit Register file

--- classes/Register.php
+++ classes/Register.php
@@ -2,6 +2,14 @@
 
     include_once '../lib/Database.php';
     include_once '../helpers/Format.php';
+
+    include_once'../PHPmailer/PHPMailer.php';
+    include_once'../PHPmailer/SMTP.php';
+    include_once'../PHPmailer/Exception.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
     
 
     class Register{
@@ -16,6 +24,35 @@
         }
 
         public function AddUser($data){
+
+            function sendemail_varifi($name, $email, $v_token){
+                $mail = new PHPMailer(true);
+                $mail->isSMTP();
+                $mail->SMTPAuth   = true;
+
+                $mail->Host       = 'smtp.example.com';
+                $mail->Username   = 'shoyabmolla420@gmail.com'; 
+                $mail->Password   = 'Molla420'; 
+
+                $mail->SMTPSecure = 'tls';  
+                $mail->Port       = 587; 
+
+                $mail->setFrom('shoyabmolla420@gmail.com', $name);
+                $mail->addAddress($email);
+
+                $mail->isHTML(true);   
+                $mail->Subject = 'Email Verification form web master';
+
+                $email_template = "
+                <h2> You have rigister web master</h2>
+                <h5> Verifi your email address to login please click the link below </h5>
+                <a 'http://localhost/PHP-project/pweb/admin/verifi-email.php?token=$v_token'>Click Here</a>
+                ";
+
+                $mail->Body    = $email_template;
+                $mail->send();
+            };
+
             $name = $this->fr->validation($data['name']);
             $phone = $this->fr->validation($data['phone']);
             $email = $this->fr->validation($data['email']);
@@ -35,6 +72,19 @@
                     $error = "This Email Is Already Exisit";
                     return $error;
                     header('location:register.php');
+                }else{
+                    $insert_query = "INSERT INTO tbl_user(name, email, phone, password, v_token) value ('$name', '$email', '$phone', '$password', '$v_token')";
+
+                    $insert_row = $this->db->insert($insert_query);
+
+                    if ($insert_row) {
+                        sendemail_varifi($name, $email, $v_token);
+                        $success = "Resistration successful. Please check Your Email inbox for varifi email";
+                        return $success;
+                    }else{
+                        $error = "Registration Failed";
+                        return $error;
+                    }
                 }
             }
         }
